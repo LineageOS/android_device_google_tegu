083c73900f85b1dd8b6c70b1076a1352e8df16bc
Michael Bestas <mkbestas@lineageos.org>
2025-11-12 23:58:16 +0200

    tegu: Move device configuration to vendor
    Change-Id: I2a4825533d1381f3903e549f1ced4d2da029d27e

--- location/device-gnss.mk
+++
@@ -1,9 +0,0 @@
-# include customized gps config files
-PRODUCT_COPY_FILES += \
-    device/google/tegu/location/ca.pem:vendor/etc/gnss/ca.pem
-
-PRODUCT_COPY_FILES += \
-    device/google/tegu/location/gps_user.cfg:vendor/etc/gnss/gps.cfg
-
-# sepolicy
-BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/gps/lsi/sepolicy
