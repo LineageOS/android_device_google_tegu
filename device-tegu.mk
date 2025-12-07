2ce3b070b5ed650ef2d17829d8a3b662d741a139
Michael Bestas <mkbestas@lineageos.org>
2025-12-07 03:50:23 +0200

    tegu: Shuffle vendor properties
    Change-Id: Idead8ca4f490071c2ba4066b10a6928d54861875

--- device-tegu.mk
+++ device-tegu.mk
@@ -34,12 +34,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.prebuilt.xml \
     android.hardware.bluetooth_le.prebuilt.xml
 
-# Touch
-PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.touch.dump.sys=/sys/devices/platform/111d0000.spi/spi_master/spi20/spi20.0/synaptics_tcm.0/sysfs
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.ignore_hdr_camera_layers=true
-
 # Recovery files
 PRODUCT_COPY_FILES += \
     device/google/tegu/recovery/init.recovery.device.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.tegu.rc
@@ -64,15 +58,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
 	frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
 
-# lhbm peak brightness delay: decided by kernel
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += vendor.primarydisplay.lhbm.frames_to_reach_peak_brightness=0
-
-# Bluetooth HAL
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.bluetooth.a2dp_offload.supported=true \
-    persist.bluetooth.a2dp_offload.disabled=false \
-    persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac-opus
-
 # Bluetooth OPUS codec
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.bluetooth.opus.enabled=true
@@ -81,11 +66,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.bluetooth.finder.supported=true
 
-# DCK properties based on target
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.gms.dck.eligible_wcc=2 \
-    ro.gms.dck.se_capability=1
-
 # Bluetooth AAC VBR
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.bluetooth.a2dp_aac.vbr_supported=true
@@ -93,14 +73,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Bluetooth Super Wide Band
 PRODUCT_PRODUCT_PROPERTIES += \
     bluetooth.hfp.swb.supported=true
-
-# default BDADDR for EVB only
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.vendor.bluetooth.evb_bdaddr="22:22:22:33:44:55"
-
-# HdMic Audio
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.app.audio.gsenet.version=1
 
 # Bluetooth LE Audio
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -143,10 +115,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
           ro.com.google.ime.kb_pad_port_b=8 \
           ro.com.google.ime.height_ratio=1.09
 
-# Audio CCA property
-PRODUCT_PROPERTY_OVERRIDES += \
-	persist.vendor.audio.cca.enabled=false
-
 # Override BQR mask to enable LE Audio Choppy report, remove BTRT logging
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.bluetooth.bqr.event_mask=295006 \
@@ -170,51 +138,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     SettingsTeguOverlay
 
-# For GPS property
-PRODUCT_VENDOR_PROPERTIES += ro.vendor.gps.pps.enabled=true
-
-PRODUCT_VENDOR_PROPERTIES += \
-	persist.device_config.configuration.disable_rescue_party=true
-
-# OIS with system imu
-PRODUCT_VENDOR_PROPERTIES += \
-    persist.vendor.camera.ois_with_system_imu=true
-
-# Allow external binning setting
-PRODUCT_VENDOR_PROPERTIES += \
-    persist.vendor.camera.allow_external_binning_setting=true
-
-# Camera Vendor property
-PRODUCT_VENDOR_PROPERTIES += \
-    persist.vendor.camera.front_720P_always_binning=true
-
-# Enable camera exif model/make reporting
-PRODUCT_VENDOR_PROPERTIES += \
-    persist.vendor.camera.exif_reveal_make_model=true
-
-# Media Performance Class 13
-PRODUCT_PROPERTY_OVERRIDES += ro.odm.build.media_performance_class=33
-
-# Display function property settings
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    vendor.display.lbe.supported=1 \
-    ro.vendor.primarydisplay.google-tg4a.temperature_path=/dev/thermal/tz-by-name/disp_therm/temp \
-    ro.vendor.display.read_temp_interval=30
-
-# Vibrator HAL
-PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.vibrator.hal.f0.comp.enabled=1 \
-    ro.vendor.vibrator.hal.redc.comp.enabled=0 \
-    persist.vendor.vibrator.hal.context.enable=false \
-    persist.vendor.vibrator.hal.context.scale=40 \
-    persist.vendor.vibrator.hal.context.fade=true \
-    persist.vendor.vibrator.hal.context.cooldowntime=1600 \
-    persist.vendor.vibrator.hal.context.settlingtime=5000
-
-# Override Output Distortion Gain
-PRODUCT_VENDOR_PROPERTIES += \
-    vendor.audio.hapticgenerator.distortion.output.gain=0.29
-
 # Setup Wizard device-specific settings
 PRODUCT_PRODUCT_PROPERTIES += \
     setupwizard.feature.enable_quick_start_flow=true
@@ -224,24 +147,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.quick_start.oem_id=00e0 \
     ro.quick_start.device_id=tegu
 
-# PKVM Memory Reclaim
-PRODUCT_VENDOR_PROPERTIES += \
-    hypervisor.memory_reclaim.supported=1
-
-# Fingerprint exposure compensation
-PRODUCT_VENDOR_PROPERTIES += \
-    persist.vendor.udfps.als_feed_forward_supported=true \
-    persist.vendor.udfps.auth_filter.mt_filter.enabled=true \
-    persist.vendor.udfps.boost_whole_auth_path_supported=true \
-    persist.vendor.udfps.capture_retrying_acquired_msg_supported=false \
-    persist.vendor.udfps.fps_touch_handler_supported=false \
-    persist.vendor.udfps.fps_touch_handler.handle_down_up_events=true \
-    persist.vendor.udfps.fth.touch_resolution_scaling_factor=10 \
-    persist.vendor.udfps.lhbm_controlled_in_hal_supported=true \
-    persist.vendor.udfps.max_touch_filter_supported=true \
-    persist.vendor.udfps.set_lhbm_in_advance=true \
-    persist.vendor.udfps.auto_exposure_compensation_supported=true
-
 # Telephony Satellite Feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.satellite.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.satellite.xml
@@ -249,19 +154,6 @@ PRODUCT_COPY_FILES += \
 # Set support for LEA multicodec
 PRODUCT_PRODUCT_PROPERTIES += \
     bluetooth.core.le_audio.codec_extension_aidl.enabled=true
-
-# Enable APF by default
-PRODUCT_VENDOR_PROPERTIES += \
-    vendor.powerhal.apf_disabled=false \
-    vendor.powerhal.apf_enabled=true
-
-# Increment the SVN for any official public releases
-PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=18
-
-# Set device family property for SMR
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.device_family=CM4KM4TK4TG4
 
 # ANGLE - Almost Native Graphics Layer Engine
 PRODUCT_PACKAGES += \
