eeac3186ef3d19c9cad2bca2b3a73a74679b07d5
Michael Bestas <mkbestas@lineageos.org>
2026-06-25 18:16:03 +0300

    tegu: Copy default_volume_tables.xml
    Change-Id: I72bb6bbc7525c07f586c8924cbf79f19cc5246a6

--- device-tegu.mk
+++ device-tegu.mk
@@ -21,6 +21,10 @@ SHIPPING_API_LEVEL := 35
 # Inherit from zumapro
 include device/google/zumapro/common.mk
 
+# Audio
+PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.allocator@1.0-service \
